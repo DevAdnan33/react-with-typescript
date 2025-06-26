87f73c388a9720e877181ec928a2946e3963d02d
Adnan Rashid <nisarbhutto180@gmail.com>
2025-06-26 14:51:55 +0500

    State Management 1.0

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,23 +1,18 @@
-import {MouseEvent} from 'react'
-
-let items = ['New York', 'Karachi', 'Islamabad', 'Daharki', 'Lahore']
-
-// function getMessage() {
-//     return items.length === 0 ? <h1>No items found</h1> : null
-// }
-
-const handleClick = (event : MouseEvent) => console.log(event)
+import { MouseEvent, useState } from 'react'
 
 function ListGroup() {
+
+    let items = ['New York', 'Karachi', 'Islamabad', 'Daharki', 'Lahore']
+    const [selectedItem, setSelectedItem] = useState(-1)
 
     return <>
         {items.length === 0 && <h1>No items found</h1>}
         <ul className="list-group">
-            {items.map((item,index) => 
-                <li 
-                    key={item} 
-                    className="list-group-item" 
-                    onClick={handleClick} >{item}
+            {items.map((item, index) =>
+                <li
+                    key={item}
+                    className={selectedItem === index ? "list-group-item active" : "list-group-item"}
+                    onClick={() => { setSelectedItem(index) }} >{item}
                 </li>
             )}
         </ul></>
