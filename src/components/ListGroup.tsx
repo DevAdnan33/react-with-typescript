9faebddd898cf13de67b5d38cdf432ed38ab9501
Adnan Rashid <nisarbhutto180@gmail.com>
2025-06-26 12:26:22 +0500

    Conditional Rendering 1.0

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,7 +1,10 @@
-const items = ['New York', 'Karachi', 'Islamabad', 'Daharki', 'Lahore']
-
+let items = ['New York', 'Karachi', 'Islamabad', 'Daharki', 'Lahore']
+items = [] 
+let message = (items.length === 0 ? <h1>No items found</h1> : null)
 function ListGroup() {
+
     return <>
+        {message}
         <ul className="list-group">
             {items.map(item => <li key={item} className="list-group-item">{item}</li>)}
         </ul></>
